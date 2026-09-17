91b0ebb47972f599ec87a0cc0f9b4f4d4c003694
Batya Sofer <b0556795806@gmail.com>
2026-09-17 14:03:40 +0300

    Target Android API 36 for Play Console

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,14 +15,14 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.mealplanner1234.app"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.mealplanner1234.app"
         minSdk = 26
-        targetSdk = 35
-        versionCode = 5
-        versionName = "1.0.4"
+        targetSdk = 36
+        versionCode = 6
+        versionName = "1.0.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
     }
